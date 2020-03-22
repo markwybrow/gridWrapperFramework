b729d05a976d8ae7b8873f866647e2dc231f53e0
Mark Wybrow <markwybrow@users.noreply.github.com>
2020-03-22 20:48:39 +1100

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
+// gridWrapping container
 import {
 	MainWrapper,
 	RowContainerWrapper,
 	ContainerPanel,
+  gridOptions,
+  gridOptionsMainWrapper
 } from './common/containers';
+
 import '../static/styles/App.scss';
+
+const containerStyles = {
+  padding: `1em 2em`,
+  margin: 0,
+  border: `1px solid lime`
+}
 
 function App() {
 	return (
 		<div className="App">
-			<MainWrapper>
+			<MainWrapper gridOptions={gridOptionsMainWrapper}>
 				{/* row one */}
-				<RowContainerWrapper gridColSizeItems={[3, 1, 1]}>
+				<RowContainerWrapper gridColSizeItems={[[2, 1], [2, 1], [1]]} gridOptions={gridOptions}>
 					<ContainerPanel
 						colSize="2"
-						title="This is a 2 column stretch"></ContainerPanel>
+						title="This is a 2 column stretch row 1" styles={containerStyles}>
+            this is my container text</ContainerPanel>
 					<ContainerPanel
 						colSize="1"
-						title="This is a 1 column"></ContainerPanel>
-					<ContainerPanel
-						colSize="1"
-						title="This is a 1 column"></ContainerPanel>
+						title="This is a 1 column row 1" styles={{padding:'1em 2em'}}></ContainerPanel>
+
 				</RowContainerWrapper>
-				<RowContainerWrapper gridColSizeItems={[4 ]}>
+				<RowContainerWrapper gridColSizeItems={[[1,1,1],[1],[1]]} gridOptions={gridOptions}>
 					<ContainerPanel
 						colSize="2"
 						title="This is a 2 column stretch"></ContainerPanel>
